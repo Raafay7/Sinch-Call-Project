40bffc35f607689d2af0033be3eab8fd737d3f4a
Raafay7 <qasimmohammad2003@gmail.com>
2024-09-12 17:32:49 +0500

    Update fetch request URL and deploy API to Vercel

--- public/script.js
+++ public/script.js
@@ -55,7 +55,7 @@ function makeCall() {
         phoneError.textContent = ''; // Clear any previous error
     }
 
-    fetch('https://call-project-seven.vercel.app/api/makeCall', {  // Correct API path for Vercel
+    fetch('/makeCall', {  // Correct API path for Vercel
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
